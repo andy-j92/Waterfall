a218afb57ef2d1fc44f30041dd837fb853e0e8b7
Suhan Muppavaram <suhan.m@hotmail.com>
2017-09-01 16:38:41 +1200

    test extraction

--- Server/server.py
+++ Server/server.py
@@ -170,7 +170,7 @@ if __name__ == '__main__':
           
         '/static': {
             'tools.staticdir.on': True,
-            'tools.staticdir.dir': './public'
+            'tools.staticdir.dir': 'public/'
         }
     }
 
