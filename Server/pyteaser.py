eba1ec68e9abd52d513bbda100d19787edb04a9a
Andy Jang <andyjang0530@gmail.com>
2017-09-08 10:55:45 +1200

    summary sentences only containing keywords

--- Server/pyteaser.py
+++ Server/pyteaser.py
@@ -105,9 +105,9 @@ def Summarize(text, keyword = None):
     #score setences, and use the top 5 sentences
     ranks = score(sentences, keys).most_common(3)
     for rank in ranks:
-        #summaries.append(rank[0])
-        sentence = re.sub(r'[^\x00-\x7f]',r'', rank[0])
-        summaries.append(sentence)
+        for keyword in split_words(keyword):
+            if keyword in rank[0].lower():
+                summaries.append(rank[0])
     return summaries
 
 
