6cc1d007ec5e586b73cf913ba3e1363a518d039b
kevin0756 <kl0756@gmail.com>
2017-09-22 11:55:25 +1200

    Implemented the feature of on click summary search for extracted keywords, separated js files from html

---
+++ Server/Public/js/ExtractText.js
@@ -0,0 +1,100 @@
+$('.loading').hide();
+$('#extractText').on('click',function(e){
+	try{
+	$('.loading').show();
+	setTimeout(function(){
+		var obj={};
+		for(i=0;i<sessionStorage.length;i++){
+			if(sessionStorage.key(i).indexOf('_smry')<0){
+			var data = new FormData();
+			data.append('data', sessionStorage.getItem(sessionStorage.key(i)));
+
+			var ourRequest = new XMLHttpRequest();
+			ourRequest.open('POST', "/extractKeywords", false);
+
+			ourRequest.onreadystatechange = function() {
+				if (this.readyState == 4 && this.status == 200) {
+					obj[sessionStorage.key(i) + "_kywrd"]=ourRequest.responseText;
+				}
+            };
+			ourRequest.send(data);
+            }
+			
+		}
+		$('.loading').hide();
+		
+		$('.list-group').empty();
+
+        var mainString = [];
+		for (var key in obj) {
+            mainString = obj[key];
+            console.log(mainString);
+            var subString = mainString.split(',');
+            for(var line in subString) {
+            	var keyWords;
+            	if(subString[line]!=''){
+                var categoryAndKeywords = subString[line].split('&');
+                console.log(categoryAndKeywords[0]);
+                console.log(categoryAndKeywords[1]);
+                var cat = categoryAndKeywords[0];
+                var keyWordsList = categoryAndKeywords[1];
+                
+                keyWords = keyWordsList.split(':');
+                
+                var clickableKeywords = "";
+                for(var keyword in keyWords) {
+                    var temp = '<a href="#" class="containedKeywords">' + keyWords[keyword] + ' </a>';
+                    clickableKeywords += temp;
+                }
+                $('.list-group').append('<p class="list-group-item" customId=' + "keyword_" +  i + '><strong>' + cat + '</strong><br>' + clickableKeywords + '</p><br><br>')
+            	}
+            }
+            
+			/*$('.list-group').append('<p class="list-group-item" customId=' + "keyword_" +  i + '><strong>' + key.substring(0,key.lastIndexOf("_kywrd")) + '</strong><br>' + obj[key] + '</p>')*/
+		}
+		
+	},15);
+	
+	
+	}catch(err){
+		console.log('An error occured ' + err);
+	}
+	
+});
+
+$(document).on("click", '.containedKeywords', function(event) {
+	var globalExtractObj=searchWithKeywords($(this).text().replace(/^\s+|\s+$/g, ""));
+	
+	sessionStorage.setItem('extractVar','true');
+	sessionStorage.setItem('extractVarObj',JSON.stringify(globalExtractObj));
+	window.location.href='/keywordsearch';
+});
+
+function searchWithKeywords(keywords){
+	var obj={};
+	for(i=0;i<sessionStorage.length;i++){
+		if(sessionStorage.key(i).indexOf('_smry')<0 && sessionStorage.key(i).indexOf('extractVar')<0){
+		var data = new FormData();
+		data.append('data', sessionStorage.getItem(sessionStorage.key(i)));
+		data.append('keywords', keywords);
+
+
+
+		var ourRequest = new XMLHttpRequest();
+		ourRequest.open('POST', "/fetchFilteredSummaries", false);
+
+		ourRequest.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				if(ourRequest.responseText!='Empty File')
+				obj[sessionStorage.key(i) + "_smry"]=ourRequest.responseText;
+
+			}else{
+
+			}
+
+		};
+		ourRequest.send(data);
+	}
+	}
+	return obj;
+}
---
+++ Server/Public/js/select-file.js
@@ -0,0 +1,136 @@
+$('.loading').hide();
+sessionStorage.removeItem('extractVar');
+sessionStorage.removeItem('extractVarObj');
+	if(sessionStorage.length){
+		if($('#filesUploadedStatus').text()=='No Files Uploaded'){
+			$('#filesUploadedStatus').text('Files Uploaded');
+		}
+		for(i=0;i<sessionStorage.length;i++){
+			if(sessionStorage.key(i).indexOf('_smry')<0){
+			$('.list-group').append('<a href="#" class="list-group-item" customId=' + "list_" +  i + '>' + sessionStorage.key(i) + '<button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button></a>');
+			}
+		} 
+		
+		
+	}
+	setActiveFile();
+	
+		 $('#buttonSubmit').on('click',function(e){
+			 $('.loading').show(); 
+			 setTimeout(function(){
+				
+				 $('#errorText').text(''); //Initially clear previous errors if present
+			        var isDuplicateFile=false;
+					var file=$('input[name="myFile"]').prop('files')[0];
+					var fileName=file.name;
+											
+					
+					var fileExt=fileName.substring(fileName.lastIndexOf('.')+1,fileName.length);
+					if('undefined'!=fileExt && ''!=fileExt){
+						if('pdf'==fileExt || 'pptx'==fileExt || 'docx'==fileExt || 'ppt'==fileExt || 'doc'==fileExt || 'txt'==fileExt){
+
+							var data = new FormData();
+							data.append('myFile', file);
+
+
+
+							var ourRequest = new XMLHttpRequest();
+							ourRequest.open('POST', "/result", false);
+
+							ourRequest.onreadystatechange = function() {
+								
+								if (this.readyState == 4 && this.status == 200) {
+									sessionStorage.setItem(fileName, ourRequest.responseText);
+									$('.loading').hide();
+								}else{
+									sessionStorage.setItem(fileName, 'Empty File');
+									$('.loading').hide();
+								}
+
+							};
+							ourRequest.send(data);
+						}else
+						$('#errorText').text('Invalid File Type!');
+						isDuplicateFile=checkDuplicateFile(isDuplicateFile,fileName); //prevents duplicate filename from being appended, however the duplicate file will replace the old file in the session!!!
+						if(!isDuplicateFile && !$('#errorText').text()){
+							if($('#filesUploadedStatus').text()=='No Files Uploaded'){
+								$('#filesUploadedStatus').text('Files Uploaded');
+							}
+							$('.list-group').append('<a href="#" class="list-group-item" customId=' + "list_" +  (sessionStorage.length) + '>' + fileName + '<button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button></a>');
+							setActiveFile();
+						    }
+					}
+ 					
+			 },15);
+ 			 
+			  
+			
+		});
+		 
+		 function setActiveFile(){
+				
+					$('.list-group-item').on('mouseover',function(){
+						$('.list-group-item').removeClass('active');
+						$('.list-group-item[customId=' + $(this).attr('customId') + ']').addClass('active');
+					});
+					
+					$('.list-group-item').on('mouseout',function(){
+						$(this).removeClass('active');
+					});
+			 }
+		 
+		 function checkDuplicateFile(param,fileName){
+			 $('.list-group').find('.list-group-item').each(function(){
+				 var listItem=$(this).text().substring(0,$(this).text().length-1);
+				 if(listItem==fileName){
+					 param=true;
+					 return false;
+				 }
+				 });
+				 
+			 return param;
+		 }
+	$('#buttonSummarize').on('click',function(e){
+		var obj={};
+		
+		var iterationLength=sessionStorage.length;
+			for(i=0;i<iterationLength;i++){
+				if(sessionStorage.key(i).indexOf('_smry')<0){
+				var data = new FormData();
+				data.append('data', sessionStorage.getItem(sessionStorage.key(i)));
+				data.append('keywords', '');
+
+
+
+				var ourRequest = new XMLHttpRequest();
+				ourRequest.open('POST', "/fetchFilteredSummaries", false);
+
+				ourRequest.onreadystatechange = function() {
+					if (this.readyState == 4 && this.status == 200) {
+						obj[sessionStorage.key(i) + "_smry"]=ourRequest.responseText;
+						
+					}else{
+						obj[sessionStorage.key(i) + "_smry"]="The file is empty";
+					}
+
+				};
+				ourRequest.send(data);
+			}
+			}
+			for (var key in obj) {
+				sessionStorage.setItem(key, obj[key]);
+				}
+			
+			window.location.href='/keywordsearch';
+	});
+
+	$(document).on("click", '.close', function(event) {  //delete file
+		var fileToRemove=$(this).parents('a').text();
+		fileToRemove=fileToRemove.substring(0,fileToRemove.length-1); //x button text also appears
+		sessionStorage.removeItem(fileToRemove);
+		sessionStorage.removeItem(fileToRemove + "_smry");
+		$(this).parents('a').remove();
+		if(!$('.list-group-item').length)
+			$('#filesUploadedStatus').text('No Files Uploaded');
+			
+	});
