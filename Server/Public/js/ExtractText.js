66ab3ae264250440aebe5f83cda948e2f681d52e
Nysird <slee559@aucklanduni.ac.nz>
2017-10-10 21:02:45 +1300

    Update and check keyword when more files uploaded, also fix keyword session storage

--- Server/Public/js/ExtractText.js
+++ Server/Public/js/ExtractText.js
@@ -1,10 +1,24 @@
 $('.loading').hide();
+
+//Check if there is keyword extracted before
+var isExtracted = false;
+for(i = 0; i < sessionStorage.length; i++){
+	if(sessionStorage.key(i).indexOf('_smry') < 0 && sessionStorage.key(i).indexOf('_keyword') < 0) {
+		var fullKeywords = sessionStorage.getItem(sessionStorage.key(i) + "_keyword");
+    	if(fullKeywords == null){
+    		isExtracted = true;
+    		break;
+    	}
+	}
+}
+if(isExtracted){ //Not extracted, extract
+	
 	try{
 		$('.loading').show();
 		setTimeout(function(){
 			var obj={};
 			for(i = 0; i < sessionStorage.length; i++){
-				if(sessionStorage.key(i).indexOf('_smry')<0){
+				if(sessionStorage.key(i).indexOf('_smry') < 0 && sessionStorage.key(i).indexOf('_keyword') < 0){
 					var data = new FormData();
 					data.append('data', sessionStorage.getItem(sessionStorage.key(i)));
 
@@ -14,14 +28,10 @@ $('.loading').hide();
 					ourRequest.onreadystatechange = function() {
 						if (this.readyState == 4 && this.status == 200) {
 							obj[sessionStorage.key(i)]=ourRequest.responseText;
-							
-							
-							
 						}
 					};
 					ourRequest.send(data);
 				}
-
 			}
 			$('.loading').hide();
 
@@ -53,7 +63,12 @@ $('.loading').hide();
 									}
 									clickableKeywords += temp;
 								}
-								$('.list-group').append('<p class="list-group-item" customId=' + "keyword_" +  i + '><strong>' + cat + '</strong><br>' + clickableKeywords + '</p>')
+								$('.list-group').append('<p class="list-group-item" customId=' + "keyword_" +  i + '><strong>' + cat + '</strong><br>' + clickableKeywords + '</p>');
+								if(sessionStorage.getItem(key+"_keyword") == null){
+									sessionStorage.setItem(key+"_keyword", '<p class="list-group-item" customId=' + "keyword_" +  i + '><strong>' + cat + '</strong><br>' + clickableKeywords + '</p>');
+								}else{
+									sessionStorage.setItem(key+"_keyword", sessionStorage.getItem(key+"_keyword") + "@#$%^&" + '<p class="list-group-item" customId=' + "keyword_" +  i + '><strong>' + cat + '</strong><br>' + clickableKeywords + '</p>');
+								}
 								i++;
 							}
 						}
@@ -62,15 +77,23 @@ $('.loading').hide();
 				
 				/*$('.list-group').append('<p class="list-group-item" customId=' + "keyword_" +  i + '><strong>' + key.substring(0,key.lastIndexOf("_kywrd")) + '</strong><br>' + obj[key] + '</p>')*/
 			}
-			for (var key in obj) {
-				sessionStorage.setItem(key+"_keyword", obj[key]);
-			}
 		},15);
-
-
 	}catch(err){
 		console.log('An error occured ' + err);
 	}
+}else{ //Just display
+	console.log("Just display");
+	for(i = 0; i < sessionStorage.length; i++){
+		if(sessionStorage.key(i).indexOf('_smry') < 0 && sessionStorage.key(i).indexOf('_keyword') < 0) {
+			var clickableKeyword = sessionStorage.getItem(sessionStorage.key(i) + "_keyword");
+			$('.list-group').append('<br><h2 id="titleText">' + sessionStorage.key(i) + '</h2><br>');
+			var clickableKeywords = clickableKeyword.split("@#$%^&");
+			for(j = 0; j < clickableKeywords.length; j++){
+				$('.list-group').append(clickableKeywords[j]);
+			}
+		}
+	}
+}
 
 $(document).on("click", '.containedKeywords', function(event) {
 	var globalExtractObj=searchWithKeywords($(this).text().replace(/^\s+|\s+$/g, ""));
